75d51ef856b3a58f3cc3df58f177e132c8402b9f
tanjung anggraini agustin <108222692+4321224@users.noreply.github.com>
2023-03-23 11:02:58 +0000

    models product for server

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,3 +1,31 @@
-import mongoose from 'mongoose';
-import {loadType} from 'mongoose-currency';
+import mongoose from "mongoose";
+import {loadType} from "mongoose-currency";
+
 const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const productSchema = new Schema({
+    price: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => (v / 100).toFixed(2),
+    },
+    expense: {
+        type: mongoose.Types.Currency,
+        currency: "USD",
+        get: (v) => (v / 100).toFixed(2),
+    },
+    transactions: [
+        {
+            type: mongoose.Schema.type.ObjectId,
+            ref: "Transaction",
+        }
+    ],
+},
+{
+    timestamps: true,toJSON: { getters: true },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
